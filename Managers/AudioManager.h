36b1baf4ba6582b26d4b27fba96495d4ff16c57c
Ted Sharygin <sharygin.t@digipen.edu>
2020-10-23 23:31:57 -0700

    started audio

---
+++ Managers/AudioManager.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef _AMAN_H_
+#define _AMAN_H_
+
+#include "Manager.h"
+
+
+class AudioManager : public Manager
+{
+public:
+	AudioManager();
+	virtual ~AudioManager();
+
+	/// Maybe(?) do not override these functions
+	/// depends on what I'll need to do
+	virtual void Update(float dt) override;
+	virtual void FrameStart() override;
+	virtual void FrameEnd() override;
+
+	virtual void Initialize() override;
+	virtual void End() override;
+public:
+private:
+private:
+};
+
+
+
+
+#endif
+
